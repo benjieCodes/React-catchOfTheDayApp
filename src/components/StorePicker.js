0a71c563e00dfd8caf8569a09609821707629851
Benjie <benjiecodes@gmail.com>
2018-04-02 15:58:38 -0400

    added JSX code to Store Picker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p> I am the Store Picker Component!</p>
+        return (
+            <React.Fragment>
+                <form className="store-selector">
+                    {/* This is a store component */}
+                    <h2>Please Enter A Store</h2>
+                    <input type="text" required placeholder="Store Name" />
+                    <button type="submit"> Visit Store →</button>
+                </form>
+            </React.Fragment>
+        )
     }
 }
 
