0ce3e5c352ddf40013cc5f4b5a5e6f9de73b3303
Benjie <benjiecodes@gmail.com>
2018-04-03 16:54:51 -0400

    added styling to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,18 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <h1>Catch of the Day</h1>
-        <h3>Fresh Food Daily!</h3>
+      <header className="top" r>
+        <h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">Of</span>
+            <span className="the">The</span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagline">
+          <span>Fresh Food Daily!</span>
+        </h3>
       </header>
     );
   }
