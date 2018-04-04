3510273ef58bb33123b986a17da4c1e25bed6e26
Benjie <benjiecodes@gmail.com>
2018-04-04 17:27:11 -0400

    imported getFunName func from helper JS to the StorePicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <form className="store-selector">
-                    {/* This is a store component */}
-                    <h2>Please Enter A Store</h2>
-                    <input type="text" required placeholder="Store Name" />
-                    <button type="submit"> Visit Store →</button>
-                </form>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <form className="store-selector">
+          {/* This is a store component */}
+          <h2>Please Enter A Store</h2>
+          <input
+            type="text"
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
+          <button type="submit"> Visit Store →</button>
+        </form>
+      </React.Fragment>
+    );
+  }
 }
 
 export default StorePicker;
