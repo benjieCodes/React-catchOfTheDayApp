a02e9084554c85953b765e71f470223a39d3b3c5
Benjie <benjiecodes@gmail.com>
2018-04-05 15:16:25 -0400

    created ref on the input tag by binding goToStore method to StorePicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,9 +2,18 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  myInput = React.createRef();
+
   goToStore(event) {
     // 1. Stop form from submitting/resetting page
     event.preventDefault();
+    // 2. get the text from my input in the form
+    console.log(this.myInput.value.value);
   }
   render() {
     return (
@@ -14,6 +23,7 @@ class StorePicker extends React.Component {
           <h2>Please Enter A Store</h2>
           <input
             type="text"
+            ref={this.myInput}
             required
             placeholder="Store Name"
             defaultValue={getFunName()}
