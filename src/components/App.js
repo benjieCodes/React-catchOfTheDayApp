40cd0d371cfbb7b7aaf0cb8575931676692828f2
Benjie <benjiecodes@gmail.com>
2018-04-05 15:36:14 -0400

    created a form to add fish in the inventory (AddFishForm component)

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Menu!" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory />
       </div>
     );
   }
