a55d3342d0342b4c6eb83117734efc0a7fbdc7e6
Benjie <benjiecodes@gmail.com>
2018-04-05 14:58:13 -0400

    added an event on the form tag of StorePicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,10 +2,14 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    // 1. Stop form from submitting/resetting page
+    event.preventDefault();
+  }
   render() {
     return (
       <React.Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           {/* This is a store component */}
           <h2>Please Enter A Store</h2>
           <input
