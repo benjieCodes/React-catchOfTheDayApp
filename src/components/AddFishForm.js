22eb0c98f80d19a25ad5e4d3b87e2e079afc9812
Benjie <benjiecodes@gmail.com>
2018-04-05 15:43:21 -0400

    added jsx form to AddFishForm component

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
+  createFish = event => {
+    event.preventDefault();
+    console.log("fish added");
+  };
+
   render() {
     return (
-      <form>
-        <p>it works</p>
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" type="text" placeholder="Name" />
+        <input name="price" type="text" placeholder="Price" />
+        <select name="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name="desc" type="text" placeholder="Description" />
+        <input name="image" type="text" placeholder="Image" />
+        <button type="submit"> + Add Fish</button>
       </form>
     );
   }
