091403f2343c56d17b6a878974233d5adec1d5d0
Benjie <benjiecodes@gmail.com>
2018-04-10 11:35:36 -0400

    in order for live state editing, need to create duplicate fish object from EditFishFrom to Inventory to App

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class EditFishForm extends React.Component {
   handleChange = event => {
-    console.log(event);
+    console.log(event.currentTarget.value);
   };
   render() {
     return (
